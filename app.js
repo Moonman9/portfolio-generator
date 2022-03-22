edeb1681d4b5505d2acf8f00b71d0eb042ae94e7
Moonman9 <zzepold97@gmail.com>
2022-03-21 18:47:53 -0700

    allows user to input two variables name and github username; and create a html page with a clickable link.

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const generatePage = require('./src/page-template.js')
 
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+
+     // for loop not for objects not set up as an array
+     // for (let i = 0; i < profileDataArr.length; i += 1) {
+     //     console.log(profileDataArr[i]);
+     // }
+// };
+
+// printProfileData(profileDataArgs);
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio comnplete! Check out index.html to see the output!')
+});
